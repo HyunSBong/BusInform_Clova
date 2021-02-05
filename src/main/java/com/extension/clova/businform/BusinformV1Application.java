50fed1ebf8be9bcd1d46dd0f6636ce4ae090fa34
HyunSBong <sanseng@mju.ac.kr>
2021-02-05 17:15:31 +0900

    init

---
+++ src/main/java/com/extension/clova/businform/BusinformV1Application.java
@@ -0,0 +1,13 @@
+package com.extension.clova.businform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinformV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinformV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/extension/clova/businform/BusinformV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extension.clova.businform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinformV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
